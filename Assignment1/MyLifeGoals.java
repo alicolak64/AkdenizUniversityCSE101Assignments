11b4b7621b29be4e45700a846dedaec512d134c4
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-07-02 13:45:42 +0300

    Added MyLifeGoals in Assignment1

---
+++ Assignment1/MyLifeGoals.java
@@ -0,0 +1,36 @@
+/**
+ * @author Ali Çolak
+ */
+
+/**
+ * 11.11.2020
+ */
+
+public class MyLifeGoals {
+
+    public static void main(String[] args) {
+
+        //Name : Ali Çolak     Date :25.10.2020
+
+        System.out.println("Short - Term  Life Goal :");
+        System.out.print("I goal to be successful in my lectures this ter  m," +
+                "especially in programming lectures.");
+        System.out.println("And I want to improve my social life " +
+                "after my university is opened.");
+
+
+        System.out.println("Medium - Term Life Goal : ");
+        System.out.print("I goal to graduate from my department with " +
+                "a good degree,good experience and new skills. ");
+        System.out.println("And I want to study master's degree abroad " +
+                " after graduate. ");
+
+
+        System.out.println("Long - Term Life Goal : ");
+        System.out.print("I goal to work where I want after graduate " +
+        "after graduating from master's degree.");
+        System.out.println("Then I want to found a startup with my friend. ");
+
+
+    }
+}
